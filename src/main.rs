8d0e0fbd834a13d276317be7fcc1c5eccbdc0f15
MTaurone <arshiahf@gmail.com>
2018-01-13 17:01:17 -0800

    Reduced room creation to a single line

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,5 @@ use text_adventure::environment::{create_rooms};
 
 fn main()
 {
-    /*
-    let mut vec_in:Vec<String> = Vec::new();
-    request_input_to_vec("Write a series of strings, separated by white space: ", &mut vec_in);
-    let mut str_in:String = String::new();
-    request_input_string("Write a string: ", &mut str_in);
-
-    for item in vec_in
-    {
-        println!("{}", item);
-    }
-    println!("{}", str_in);
-    */
-    let mut root_dir:String = String::new();
-    request_input_string("Please input root directory with no spaces: ", &mut root_dir);
-    create_rooms(&mut root_dir);
-
+    create_rooms();
 }
